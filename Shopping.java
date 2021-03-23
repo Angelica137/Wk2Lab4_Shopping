c21fdb9014d549381a8105a4dde03f330c0a12a4
Angelica <Angelica137@users.noreply.github.com>
2021-03-23 15:57:44 +0000

    create a shopping list object

---
+++ Shopping.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Shopping {
+
+	// storage for an arbitrary nu,ber of items
+	private ArrayList shoppingList;
+
+	// initialise the shopping class
+	public Shopping() {
+
+		shoppingList = new ArrayList();
+
+	}
+
+}
---
+++ ShoppingTester.java
@@ -0,0 +1,9 @@
+
+public class ShoppingTester {
+
+	public static void main(String[] args) {
+
+		// initialise a s hopping lsit
+		Shopping s = new Shopping();
+	}
+}
